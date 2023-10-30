0f16f8190dc6e8c70fd93b89efa2c9d9fa6e0174
facuseijo <136520267+facuseijo@users.noreply.github.com>
2023-10-30 01:20:40 -0300

    react1

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,9 @@
+import cart from "../assets/imagescart.png";
+
+export const CartWidget = () => {
+    return (
+    <>
+    <img src={cart} alt=" chango" widt="35" />9
+    </>
+    );
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+import { CartWidget } from "./CartWidget";
+
+export const NavBar = () => {
+    return (
+    <Navbar bg="dark" data-bs-theme="dark">
+    <Container>
+      <Navbar.Brand href="#home">Distribuidora Bristol Tienda de Comestibles </Navbar.Brand>
+       <Nav className="me-auto">
+        <Nav.Link href="#home">Salazones</Nav.Link>
+        <Nav.Link href="#features">Embutidos</Nav.Link>
+        <Nav.Link href="#pricing">Chacinados</Nav.Link>
+      </Nav>
+      <CartWidget />
+    </Container>
+    </Navbar>
+    );
+} ;
